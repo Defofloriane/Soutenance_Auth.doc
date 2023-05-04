9513c46bfa916aeecf7c297ce4aadd3b3e5a609b
Defofloriane <88293985+Defofloriane@users.noreply.github.com>
2023-05-04 13:15:09 +0200

    seeders

--- database/seeders/EtudiantSeeder.php
+++ database/seeders/EtudiantSeeder.php
@@ -14,16 +14,20 @@ class EtudiantSeeder extends Seeder
     public function run(): void
     {
         $etudiant=[
-         'matricule'=>'20R2198',
-         'nom'=>'EZO\'O',
-         'prenom'=>'DAVID GAEL',
-         'date_naissance'=>'2001/08/04'
-        ];
-        $etudiant=[
-            'matricule'=>'19K2779',
-            'nom'=>'KOUGABA',
-            'prenom'=>'MARLIN BLERIAUX',
-            'date_naissance'=>'1997/02/14'
+            [
+                'matricule'=>'20R2198',
+                'nom'=>'EZO\'O',
+                'prenom'=>'DAVID GAEL',
+                'date_naissance'=>'2001/08/04'
+            ],
+            [
+                'matricule'=>'19K2779',
+                'nom'=>'KOUGABA',
+                'prenom'=>'MARLIN BLERIAUX',
+                'date_naissance'=>'1997/02/14'
+
+            ],
+           
            ];
     Etudiant::insert($etudiant);
     }
